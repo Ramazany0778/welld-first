9362f0e4247dfeaf9ac27bf8123937d5e2222689
Admin <ramazany0778@gmail.com>
2026-04-15 13:50:42 +0200

    ho creato app base

--- basic.js
+++ basic.js
@@ -5,4 +5,60 @@ function openAlert() {
 
 }
 
+function x(){
+    alert("provo")
+}
+
+function MyButton(){
+    return( <button> My button</button>
+    );
+}
+
+export default function MyApp(){
+    return(
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton/>
+        </div>
+    );
+}
+
+<img className= "Avatar" />
+
+return (
+    <h1>
+        {user.name}
+    </h1>
+)
+
+return (
+    <img
+        className="avatar"
+        src={user.imageUrl}
+        />
+);
+
+const user = {
+    name: 'utente',
+    imageUrl: '',
+    imageSize: 90,
+
+};
+
+export default function Profile() {
+    return (
+        <>
+        <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
 
---
+++ tictactoe/src/index.js
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App.jsx";
+
+const root= createRoot(document.getElementById("root"));
+root.render(
+    <strictMode>
+        <app />
+    </strictMode>
+);
+
+/*finish*/

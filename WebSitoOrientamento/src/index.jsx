cb9ef80534183d3e6c164b95f8322a7f161af3b1
Admin <ramazany0778@gmail.com>
2026-04-17 12:11:41 +0200

    installato react-rouder e

--- WebSitoOrientamento/src/index.jsx
+++ WebSitoOrientamento/src/index.jsx
@@ -1,18 +1,52 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./styles.css";
 
 import App from "./App";
-import Footer from "./Footer";
 import Login from "./Login";
-import Menu from "./Menu";
+import ChiSiamo from "./ChiSiamo";
+import ComeLavoriamo from "./ComeLavoriamo";
+import CosaFacciamo from "./CosaFacciamo";
+import TrovaIlLavoro from "./TrovaIlLavoro";
+import CreateAccount from "./CreateAccount";
+import Profile from "./Profile";
+import TypeWork from "./TypeWork";
+import Footer from "./Footer";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
 
-    <App />,
-    <Footer />,
-    <Login />,
-    <Menu />,
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(
 
+
+    <BrowserRouter>
+        <Routes>
+            <Route path= "/" element={<App /> } />
+
+            <Route element = {"Login/Sign" } >
+                <Route path="Login" element={ <Login /> } />
+                <Route path={"Register"} element={<CreateAccount /> } />
+            </Route>
+
+            <Route path="Menu">
+                <Route path="Chi siamo" element={ <ChiSiamo /> } />
+                <Route path="comeLavoriamo" element={<ComeLavoriamo /> } />
+                <Route path="cosaFacciamo" element={<CosaFacciamo /> } />
+                <Route path="trovaIlLavoro" element={ <TrovaIlLavoro />}  />
+            </Route>
+
+            <Route path="Profile" >
+                <Route path="Profile" element={ <Profile /> }></Route>
+            </Route>
+
+            <Route path="TypeWork" >
+                <Route path="TypeWork" element={ <TypeWork /> } />
+            </Route>
+
+            <Route path="Footer" >
+                <Route path="Footer" element={ <Footer /> } />
+
+            </Route>
+        </Routes>
+    </BrowserRouter>
 );
